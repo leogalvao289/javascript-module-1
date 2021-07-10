39f7c5d1871ab2dde27bf3bb9f873fc190e95962
leogalvao289 <leogalvao289@gmail.com>
2021-07-10 10:55:50 +0200

    update

--- week-2/Homework/C-comparison-operators/exercise.js
+++ week-2/Homework/C-comparison-operators/exercise.js
@@ -5,28 +5,28 @@
   The variables should have values that match the expected results.
 */
 
-var studentCount = 16;
-var mentorCount = 9;
-var moreStudentsThanMentors = (studentCount > mentorCount); 
+var studentCount = 16
+var mentorCount = 9
+var moreStudentsThanMentors = studentCount > mentorCount
 
- 
 // finish this statement
 
- 
+var roomMaxCapacity = 25
+var enoughSpaceInRoom = roomMaxCapacity <= studentCount + mentorCount // finish this statement
 
-var roomMaxCapacity = 25;
-var enoughSpaceInRoom = (roomMaxCapacity > studentCount); // finish this statement
-
-var personA = "Daniel";
-var personB = "Irina";
-var sameName = (personA === personB); // finish this statement
+var personA = 'Daniel'
+var personB = 'Irina'
+var sameName = personA === personB // finish this statement
 
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
-console.log("Are there more students than mentors?", moreStudentsThanMentors);
-console.log("Is there enough space in the room for all students and mentors?", enoughSpaceInRoom);
-console.log("Do person A and person B have the the same name?", sameName);
+console.log('Are there more students than mentors?', moreStudentsThanMentors)
+console.log(
+  'Is there enough space in the room for all students and mentors?',
+  enoughSpaceInRoom
+)
+console.log('Do person A and person B have the the same name?', sameName)
 
 /* 
   EXPECTED RESULT
